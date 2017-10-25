febb9211564a156ca3c22b239409c975e7054b0a
Ryan <bradburyrp@gmail.com>
2017-10-25 15:24:44 -0400

    Getting the data is now done through search.php, which allows various search terms. get_all_locations.php will no longer be used.

--- search.php
+++ search.php
@@ -6,8 +6,54 @@ require_once __DIR__ . '/db_connect.php';
 
 // connect to db
 $db = new db_connect();
+$sport = $_GET["sport"];
+$city = $_GET["city"];
+$comp = $_GET["comp"];
+$fun = $_GET["fun"];
+/* echo $sport;
+echo $city;
+echo $comp;*/
+
+$first = true;//for proper sql syntax
 
 $sql = "SELECT * FROM markers";
+//echo $sql;
+
+if($sport != 'any'){
+		$sql .= " WHERE title = '$sport'";
+		$first = false;
+}
+//echo $sql;
+if($city != 'any'){
+	if($first == true){
+		$sql .= " WHERE address = '$city'";
+	}
+	else if($first == false){
+		$sql .= " AND address = '$city'";
+	}
+}
+//echo $sql;
+//echo $comp;
+if($comp == 'true' && $fun != 'true'){
+	if($first == true){
+		$sql .= " WHERE comp_level = 'Competitive'";
+	}
+	else if($first == false){
+		$sql .= " AND comp_level = 'Competitive'";
+	}
+}
+//echo $sql;
+//echo $fun;
+if($fun == 'true' && $comp != 'true'){
+	if($first == true){
+		$sql .= " WHERE comp_level = 'Fun'";
+	}
+	else if($first == false){
+		$sql .= " AND comp_level = 'Fun'";
+	}
+}
+//echo "line 55: ".$sql;
+
 //Get all markers
 $result = $db->conn->query($sql);
 //$values = $result->fetch_assoc();
@@ -27,6 +73,8 @@ if (count($result) > 0) {
 		//echo $marker["latitude"];
 		$marker["longitude"] = $row["longitude"];
 		$marker["title"] = $row["title"];
+		$marker["city"] = $row["address"];
+		$marker["comp_level"] = $row["comp_level"];
 		
 		// push single marker into final response array
         array_push($response['markers'], $marker);
