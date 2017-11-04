750a1d7ff1c5632c4953d5cec9f0df66c3a4184b
Ryan <bradburyrp@gmail.com>
2017-11-04 19:02:32 -0400

    Created file create_game.php, which is used when the user creates a game

---
+++ create_game.php
@@ -0,0 +1,31 @@
+<?php
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connect to db
+$db = new db_connect();
+$sport = $_GET["sport"];
+$city = $_GET["city"];
+$time = $_GET["time"];
+$postalAddress = $_GET["address"];
+$date = $_GET["date"];
+$comp = $_GET["comp"];
+$lat = $_GET["lat"];
+$long = $_GET["long"];
+$uid = $_GET["uid"];
+
+$gid = rand(0, 1000000);//Create a random number for use as the game's ID. This is obviously a temporary solution, with the potential for conflicts. 
+
+$sql = "INSERT INTO `location_database`.`markers` (`game_id`, `latitude`, `longitude`, `title`, `address`, `time`, `date`, `comp_level`, `postalAddress`, `user_id`) 
+VALUES ('$gid', '$lat', '$long', '$sport', '$city', '$time', '$date', '$comp', '$postalAddress', '$uid');";
+
+//$result = $db->conn->query($sql);
+if ($db->conn->query($sql) === TRUE) {
+	echo "<br/>"; // new line
+    echo "Insertion successful";
+} else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+}
