6227273f25c8690a62dc52048b42b8845c90e2bb
Ryan <bradburyrp@gmail.com>
2017-12-07 15:18:00 -0500

    Added the ability to leave games and view who's attending a game

---
+++ attendees.php
@@ -0,0 +1,42 @@
+<?php
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connect to db
+$db = new db_connect();
+$gid = $_GET["gid"];
+/* echo $sport;
+echo $city;
+echo $comp;*/
+
+$first = true;//for proper sql syntax
+
+$sql = "SELECT * FROM `attendees_$gid`";
+
+$result = $db->conn->query($sql);
+
+if (count($result) > 0) {
+    $response['markers'] = array();
+	
+	while($row = mysqli_fetch_assoc($result)){
+        // temp user array
+        $marker = array();
+		$marker['gameid'] = $row['user_id'];
+		
+		// push single marker into final response array
+        array_push($response['markers'], $marker);
+	 }
+	 // success
+    $response["success"] = 1;
+ 
+    // echoing JSON response
+    echo json_encode($response);
+} else {
+    $response["success"] = 0;
+    $response["message"] = "No markers found";
+ 
+    echo json_encode($response);
+}//https://csc-182021.appspot.com/?sport=any&city=any&comp=true&fun=false
+?>
---
+++ leavegame.php
@@ -0,0 +1,102 @@
+<?php
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connect to db
+$db = new db_connect();
+$gid = $_GET["gid"];
+$uid = $_GET["uid"];
+$role = "";
+
+$sql = "";
+$sql = "SELECT * FROM `csc400db`.`attendees_$gid` WHERE user_id = '$uid'";
+
+$result = $db->conn->query($sql);
+
+$row = mysqli_fetch_assoc($result);
+echo "<br/>";
+$role = $row['role'];
+
+if ($result === TRUE) {
+	echo "<br/>"; // new line
+    echo "Deletion successful";
+	}
+else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+	}
+
+if($role == 'player'){
+	$sql = "SELECT curr_signed FROM markers WHERE game_id = '$gid'";
+	
+	$curr_signed = $db->conn->query($sql);
+	$row = $curr_signed->fetch_assoc();
+	$curr_signed = $row["curr_signed"];
+	$curr_signed = $curr_signed - 1;
+	
+	$sql = "";
+	$sql = "UPDATE markers SET curr_signed = '$curr_signed' WHERE game_id = $gid";
+	
+	$curr_signed = $db->conn->query($sql);
+	
+	if ($db->conn->query($sql) === TRUE) {
+	echo "<br/>"; // new line
+    echo "Update successful";
+	}
+	else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+	}
+	}
+	
+	if($role == 'referee'){
+	$sql = "SELECT curr_refs FROM markers WHERE game_id = '$gid'";
+	
+	$curr_refs = $db->conn->query($sql);
+	$row = $curr_refs->fetch_assoc();
+	$curr_refs = $row["curr_refs"];
+	$curr_refs = $curr_refs - 1;
+	
+	$sql = "";
+	$sql = "UPDATE markers SET curr_refs = '$curr_refs' WHERE game_id = $gid";
+	
+	$curr_signed = $db->conn->query($sql);
+	
+	if ($db->conn->query($sql) === TRUE) {
+	echo "<br/>"; // new line
+    echo "Update successful";
+	}
+	else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+	}
+	}
+
+$sql = "DELETE FROM `csc400db`.`attendees_$gid` WHERE CONCAT(`attendees_$gid`.`user_id`) = '$uid'";
+
+if ($db->conn->query($sql) === TRUE) {
+	echo "<br/>"; // new line
+    echo "Deletion successful";
+	}
+else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+	}
+
+$sql = "";
+#Localhost
+#$sql = "DELETE FROM `location_database`.`markers` WHERE CONCAT(`markers`.`game_id`) = $gid";
+
+#GCP
+$sql = "DELETE FROM `csc400db`.`gamelist_$uid` WHERE CONCAT(`gamelist_$uid`.`game_id`) = $gid";
+
+if ($db->conn->query($sql) === TRUE) {
+	echo "<br/>"; // new line
+    echo "Deletion successful";
+	}
+else {
+	echo "<br/>"; // new line 
+    echo "Error accessing database: " . $db->conn->error;
+	}
